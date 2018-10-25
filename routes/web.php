9f65ff320523b1bb069c609124f48461182f7d5f
olarucristian <olarucristi1995@gmail.com>
2018-10-25 09:35:35 +0300

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     $tasks = DB::table('tasks')->get();
     return view('welcome', compact('tasks'));
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
